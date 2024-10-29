c9c46e839acf621d54629a97d2597edbf090ed76
Hanka Celá <hana-cela@email.cz>
2024-10-29 15:42:05 +0100

    Opraveny ukol

--- script.js
+++ script.js
@@ -5,9 +5,17 @@ const vysledekElement = document.querySelector('#vysledek');
 document.querySelector('#generujEmail').addEventListener('click', function () {
     const jmeno = jmenoInput.value.trim();
     const prijmeni = prijmeniInput.value.trim();
+
+    const diacriticsPattern = /[ěščřžýáíéúůťňďöäüß]/i;
+
+    if (diacriticsPattern.test(jmeno) || diacriticsPattern.test(prijmeni)) {
+        vysledekElement.textContent = "Prosím, zadejte jméno a příjmení bez diakritiky.";
+        return; 
+    }
+
     const zkracenePrijmeni = prijmeni.slice(0, 5).toLowerCase();
     const zkraceneJmeno = jmeno.slice(0, 3).toLowerCase();
     const email = `${zkracenePrijmeni}${zkraceneJmeno}@fit.cvut.cz`;
 
-    vysledekElement.textContent += `Tvoje e-mailová adresa: ${email}`;
+    vysledekElement.textContent = `Tvoje e-mailová adresa: ${email}`;
 });
